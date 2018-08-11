044bd38353dc86b822040346b50dcaf090500e77
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-08-11 17:18:04 -0300

    unify test to class file

--- src/symbol-table.js
+++ src/symbol-table.js
@@ -1,3 +1,14 @@
 function SymbolTable() {
-    var created = true;
+    this.created = true;
 }
+
+
+// Testes
+describe('Cria tabela de símbolos', function () {
+    console.log(SymbolTable);
+    it('Foi criada', function () {
+        var table = new SymbolTable();
+        console.log(table);
+        expect(table.created).toBe(true);
+    });
+});
--- tests/symbol-table.js
+++
@@ -1,6 +0,0 @@
-describe('Cria tabela de símbolos', function () {
-    var table = true;
-    it('Foi criada', function(){
-        expect(table).toBe(true);
-    });
-});
