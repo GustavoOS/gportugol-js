5a8946ba4e62e464e39e0bca891cdce440bcfef7
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-08-11 19:55:32 -0300

    Create symbol table

--- src/symbol-table.js
+++ src/symbol-table.js
@@ -1,14 +1,72 @@
 function SymbolTable() {
-    this.created = true;
+    this.scopes = {};
+    this.addSymbol = function (name, scope = "algoritmo") {
+        if (this.scopes[scope] ) {
+            if (this.find(name, scope) === undefined) this.scopes[scope].push(name);
+        } else {
+            this.scopes[scope] = [name];
+        }
+    };
+    this.find = function (name, scope = "algoritmo") {
+        if (this.scopes[scope]) {
+            return this.scopes[scope].find((rs) => {
+                return rs === name;
+            });
+        }
+        return;
+    }
 }
 
-
 // Testes
+function Teste() {
+    var mass = {
+        names: ['Gilberto', 'Adenor', 'Damares', 'Rubi'],
+        scopes: ['algoritmo', 'sort', 'order', 'grow']
+    };
+    return mass;
+}
 describe('Cria tabela de símbolos', function () {
-    console.log(SymbolTable);
-    it('Foi criada', function () {
-        var table = new SymbolTable();
-        console.log(table);
-        expect(table.created).toBe(true);
+    var table;
+
+    beforeEach(function () {
+        table = new SymbolTable();
     });
+
+    it('Foi criada', function () {
+        expect(table).toBeDefined();
+    });
+
+    it('Verifica se os escopos estão vazios', function () {
+        expect(table.scopes).toBeDefined();
+    });
+});
+
+describe('Inserção de símbolos', function () {
+    var table;
+    var nameList = [];
+
+    beforeAll(function () {
+        nameList = Teste().names;
+    });
+
+    beforeEach(function () {
+        table = new SymbolTable();
+        table.addSymbol("Joao");
+    });
+
+    it('Verifica se inserção funcionou', function () {
+        expect(table.find("Joao")).toBeDefined();
+    });
+
+    it('Verifica múltiplas inserções', function () {
+        nameList.forEach(element => {
+            table.addSymbol(element);
+        });
+        expect(table.scopes.algoritmo).toEqual(["Joao"].concat(nameList));
+    });
+
+    it('Verifica se inserção não colide', function () {
+        table.addSymbol("Joao");
+        expect(table.scopes.algoritmo).toEqual(["Joao"]);
+    })
 });
