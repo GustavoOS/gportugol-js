eb3ed11ad498d25474e6c57d8fba862ca59b33af
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-08-11 15:55:49 -0300

    fix test settings

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.js tests/*.js'
+      'src/*.js', 'tests/*.js'
     ],
 
 
@@ -55,12 +55,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: 'Firefox',
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
