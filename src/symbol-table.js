bb718ca885d27766605e174f00ce68c2bd776471
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-08-11 21:34:52 -0300

    more complex symbol entries

--- src/symbol-table.js
+++ src/symbol-table.js
@@ -1,20 +1,51 @@
 function SymbolTable() {
     this.scopes = {};
-    this.addSymbol = function (name, scope = "algoritmo") {
-        if (this.scopes[scope] ) {
-            if (this.find(name, scope) === undefined) this.scopes[scope].push(name);
+    this.addSymbol = function (name, scope = "algoritmo", location = 0) {
+        var newSymbol = new SymbolEntry(name, scope, location);
+        if (this.scopes[scope]) {
+            var foundSymbolIndex = this.findIndex(name, scope);
+            if (foundSymbolIndex >= 0) {
+                this.scopes[scope][foundSymbolIndex].addLocation(location);
+            } else {
+                this.scopes[scope].push(newSymbol);
+            }
         } else {
-            this.scopes[scope] = [name];
+            this.scopes[scope] = [newSymbol];
         }
     };
     this.find = function (name, scope = "algoritmo") {
         if (this.scopes[scope]) {
             return this.scopes[scope].find((rs) => {
-                return rs === name;
+                return rs.name === name;
             });
         }
         return;
-    }
+    };
+    this.findIndex = function (name, scope = "algoritmo") {
+        if (this.scopes[scope]) {
+            return this.scopes[scope].findIndex((rs) => {
+                return rs.name === name;
+            });
+        }
+        return -1;
+    };
+
+}
+
+function SymbolEntry(name, scope = "algoritmo", location = 0) {
+    this.name = name;
+    this.scope = scope;
+    this.location = [location];
+
+    this.addLocation = function (local = 1) {
+        this.location.push(1);
+    };
+
+    this.unsetFunctions = function () {
+        this.addLocation = undefined;
+        this.unsetFunctions = undefined;
+        return this;
+    };
 }
 
 // Testes
@@ -24,6 +55,13 @@ function Teste() {
         scopes: ['algoritmo', 'sort', 'order', 'grow']
     };
     return mass;
+}
+
+function unsetFunctionsFromArrayOfEntries(arrayOfEntries) {
+    arrayOfEntries.forEach(
+        (el) => el.unsetFunctions()
+    );
+    return arrayOfEntries;
 }
 describe('Cria tabela de símbolos', function () {
     var table;
@@ -59,20 +97,27 @@ describe('Inserção de símbolos', function () {
     });
 
     it('Verifica múltiplas inserções', function () {
+        var expectedArray = [new SymbolEntry("Joao")];
         nameList.forEach(element => {
+            expectedArray.push(new SymbolEntry(element));
             table.addSymbol(element);
         });
-        expect(table.scopes.algoritmo).toEqual(["Joao"].concat(nameList));
+        expect(unsetFunctionsFromArrayOfEntries(table.scopes.algoritmo)).toEqual(
+            unsetFunctionsFromArrayOfEntries(expectedArray));
     });
 
     it('Verifica se inserção não colide', function () {
         table.addSymbol("Joao");
-        expect(table.scopes.algoritmo).toEqual(["Joao"]);
+        expect(table.find("Joao").location.length).toBe(2);
+        expect(table.scopes.algoritmo.length).toBe(1);
     });
 
-    it("Inserção em dois escopos e mesmo nome", function(){
+    it("Inserção em dois escopos e mesmo nome", function () {
         table.addSymbol("Joao", Teste().scopes[1]);
-        expect(table.scopes[Teste().scopes[1]]).toEqual(["Joao"]);
+        expect(unsetFunctionsFromArrayOfEntries(
+            table.scopes[Teste().scopes[1]])).toEqual(
+            [(new SymbolEntry(
+                "Joao", Teste().scopes[1])).unsetFunctions()]);
         expect(table.scopes.algoritmo.length).toBe(1);
     });
 });
