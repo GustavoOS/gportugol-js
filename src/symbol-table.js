3c96e65c482f2a0da6ffb2215fc1f99a495eeccc
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-08-12 00:37:16 -0300

    Refactor add id and type

--- src/symbol-table.js
+++ src/symbol-table.js
@@ -1,13 +1,24 @@
 function SymbolTable() {
     this.scopes = {};
-    this.declareVariable = function (name, scope = "algoritmo", location = 0) {
-        if (this.scopes[scope] === undefined) {
-            this.scopes[scope] = {};
-        }
+    this.declareVariable = function (name, scope = "algoritmo", location = 0, type = "INTEIRO") {
         if (this.scopes[scope][name] !== undefined) return false;
-        this.scopes[scope][name] = [location];
+        this.scopes[scope][name] = new SymbolEntry(
+            location,
+            type,
+            ++this.scopes[scope].variableCount);
         return true;
     };
+    this.declareScope = function (name = "algoritmo") {
+        this.scopes[name] = {
+            variableCount: 0
+        };
+    };
+}
+
+function SymbolEntry(location, type, id) {
+    this.location = [location];
+    this.type = type;
+    this.id = id;
 }
 
 
@@ -26,6 +37,7 @@ describe('Cria tabela de símbolos', function () {
 
     beforeEach(function () {
         table = new SymbolTable();
+        Teste().scopes.forEach((el) => table.declareScope(el));
     });
 
     it('Foi criada', function () {
@@ -33,55 +45,75 @@ describe('Cria tabela de símbolos', function () {
     });
 
     it('Verifica se os escopos estão vazios', function () {
-        expect(table.scopes).toBeDefined();
+        for (var scope in table.scopes) {
+            expect(table.scopes[scope].variableCount).toBe(0);
+        }
     });
 });
 
 describe('Inserção de símbolos', function () {
     var table;
     var nameList = [];
+    var scopeList = [];
 
-    beforeAll(function () {
-        table = new SymbolTable();
-        nameList = Teste().names;
-    });
+
 
     beforeEach(function () {
-        table.declareVariable("Joao");
+        table = new SymbolTable();
+        scopeList = Teste().scopes;
+        nameList = Teste().names;
+        scopeList.forEach((el) => table.declareScope(el));
     });
 
     it('Verifica se inserção funcionou', function () {
-        expect(table.scopes.algoritmo.Joao).toBeDefined();
+        table.declareVariable("Joao");
+        expect(table.scopes.algoritmo.Joao).toEqual(new SymbolEntry(0, "INTEIRO", 1));
+    });
+
+    it("Declara variáveis de tipos diferentes num mesmo escopo", () => {
+        table.declareScope("empilha");
+        table.declareVariable("stack", "empilha", 23, "CARACTERE");
+        table.declareVariable("item", "empilha", 25, "REAL");
+        expect(table.scopes.empilha.stack).toEqual(
+            new SymbolEntry(23, "CARACTERE", 1)
+        );
+        expect(table.scopes.empilha.item).toEqual(
+            new SymbolEntry(25, "REAL", 2)
+        );
     });
 
     it('Verifica múltiplas inserções', function () {
-        var allVariables = ["Joao"].concat(nameList);
-        nameList.forEach(element => {
+        nameList.forEach((element) => {
             table.declareVariable(element);
         });
-        allVariables.forEach((variable) => {
-            expect(table.scopes.algoritmo[variable]).toBeDefined();
+        nameList.forEach((variable, index) => {
+            expect(table.scopes.algoritmo[variable]).toEqual(
+                new SymbolEntry(0, "INTEIRO", index + 1)
+            );
         });
     });
 
     it('Verifica se inserção não colide', function () {
+        table.declareVariable("Joao");
         var gotDeclared = table.declareVariable("Joao");
         expect(gotDeclared).toBeFalsy();
     });
 
     it("Inserção em dois escopos e mesmo nome", function () {
-        table.declareVariable("Joao", Teste().scopes[1]);
+        table.declareVariable("Joao");
+        table.declareVariable("Joao", scopeList[1]);
         expect(table.scopes.algoritmo.Joao).toBeDefined();
-        expect(table.scopes[Teste().scopes[1]].Joao).toBeDefined();
+        expect(table.scopes[scopeList[1]].Joao).toBeDefined();
     });
 
     it("Inserção em vários escopos", () => {
-        table.scopes = {};
         nameList.forEach((element, index) => {
-            table.declareVariable(element, Teste().scopes[index]);
+            table.declareVariable(element, scopeList[index]);
         });
         nameList.forEach((element, index) => {
-            expect(table.scopes[Teste().scopes[index]][element]).toBeDefined();
+            expect(table.scopes[scopeList[index]][element]).toEqual(
+                new SymbolEntry(0, "INTEIRO", 1)
+            );
         });
     });
 });
