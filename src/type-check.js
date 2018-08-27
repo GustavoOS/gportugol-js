7b8576ee98de1414c6003486813cfc23fb9fbdff
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-08-26 22:11:14 -0300

    Remove referências não usadas

--- src/type-check.js
+++ src/type-check.js
@@ -2,8 +2,6 @@ module.exports.TypeCheck = TypeCheck;
 
 var st = require('../src/symbol-table');
 var SymbolTable = st.SymbolTable;
-var SymbolEntry = st.SymbolEntry;
-var Scope = st.Scope;
 
 function TypeCheck(ast) {
     this.ast = ast;
