5a28116188db6a2fd06b5b30f49bdf9c895bd751
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-12-25 14:28:17 -0200

    refatorar teste do type check

--- tests/type-check.spec.js
+++ tests/type-check.spec.js
@@ -4,8 +4,8 @@ var Local = require("../src/Parser/data-structure").Local;
 const tree = require("./test-ast");
 
 
-describe("Verifica tipos: ", function () {
-    var type;
+describe("Verifica inicialização da classe TypeCheck: ", function () {
+    let type;
     beforeEach(function () {
         type = new TypeCheck(tree);
     });
@@ -17,26 +17,31 @@ describe("Verifica tipos: ", function () {
     it("Verifica criação da tabela de símbolos", function () {
         expect(type.SymbolTable).toBeDefined();
     });
+});
+
+describe("Inspeção da tabela de símbolos:  ", () => {
+    let type;
+
+    beforeEach(function () {
+        type = new TypeCheck(tree);
+        type.createScopesAndDeclareVariables();
+    });
 
     it("Todos os escopos estão na tabela de símbolos", function () {
-        type.createScopesAndDeclareVariables();
         expect(type.SymbolTable.scopes.fatorial_recursivo).toBeDefined();
         expect(type.SymbolTable.scopes.fatorial).toBeDefined();
     });
 
     it("Variáveis em funções e fora de função possuem entradas", function () {
-        type.createScopesAndDeclareVariables();
         expect(type.SymbolTable.scopes.fatorial_recursivo.x.id).toEqual(1);
         expect(type.SymbolTable.scopes.fatorial.z.id).toEqual(1);
     });
 
     it("Matrizes estão armazenadas na tabela de símbolos", function () {
-        type.createScopesAndDeclareVariables();
         expect(type.SymbolTable.scopes.fatorial.m1.dimensions).toEqual([2, 4]);
     });
 
     it("Verifica inserção de local ao inserir matriz", () => {
-        type.createScopesAndDeclareVariables();
         expect(type.SymbolTable.scopes.fatorial.m1.location).toEqual([new Local(11, 11, 4, 33)]);
     });
 });
